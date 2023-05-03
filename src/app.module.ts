7e994cf073fd3dd83ed1a0440df9f114a8fbdacd
rdmaks98 <rinkymakvana7@gmail.com>
2023-05-03 17:00:21 +0530

    mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './database/config/ormConfig';
+
 
 @Module({
-  imports: [ ConfigModule.forRoot(),],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRoot(ormConfig()),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
